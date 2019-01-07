077199ae8b3651ed83afdac794304af15f366cbe
Walker <walker@incuca.com.br>
2019-01-07 14:11:17 -0200

    add plugin files

---
+++ ic-component.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     INCUCA Enfold Component
+ * Plugin URI:      https://incuca.net
+ * Description:     Enfold component
+ * Author:          INCUCA
+ * Author URI:      https://incuca.net
+ * Text Domain:     ic-component
+ * Version:         0.1.0
+ *
+ * @package         Ic_Enfold
+ */
+// Your code starts here.
